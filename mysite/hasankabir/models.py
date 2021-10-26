d0567b4761ff4fd087cfa5f0696f7a804a5114d2
ZakharB <rachet2012@yandex.ru>
2021-10-26 03:15:33 +0300

    po4ti

---
+++ mysite/hasankabir/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Database(models.Model):
+    """
+    :param qu_liq_m3day: дебит скважины по жидкости, м3/сут
+    :param d_i_m: внешний диаметр НКТ, мм
+    :param d_o_m: внутренний диаметр ЭК, мм
+    :param h: глубина скважины, м 
+    :param p_head: давление на устье скважины, атм
+    :param t_head: температура на устье скважины, С
+    :param wct: обводненность продукции, дол.ед   
+    """
+
+    qu_liq_m3day = models.FloatField()  
+    d_i_m = models.FloatField()  
+    d_o_m = models.FloatField()  
+    h = models.FloatField()  
+    p_head = models.FloatField()  
+    t_head = models.FloatField()  
+    wct = models.FloatField()  
+    rp = models.FloatField() 
+    results = models.CharField(max_length=1000000)
+
+
+    def __str__(self):
+        return self.results
+
---
+++ mysite/hasankabir/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from hasankabir.forms import DataForm
+from hasankabir.models import Database
+from hasankabir.djHK import HasanKabirAnn
+
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        form = DataForm(request.POST)
+        context['form'] = form
+
+        qu_liq_m3dayd = float(form['qu_liq_m3day'].value())
+        d_i_md  = float(form['d_i_m'].value()) 
+        d_o_md = float(form['d_o_m'].value())
+        hd = int(form['h'].value())
+        p_headd = float(form['p_head'].value()) 
+        t_headd = float(form['t_head'].value()) 
+        wctd = float(form['wct'].value())
+        rpd = float(form['rp'].value()) 
+        
+        data = Database()
+        data.qu_liq_m3day = qu_liq_m3dayd
+        data.d_i_m = d_i_md
+        data.d_o_m = d_o_md
+        data.h = hd
+        data.p_head = p_headd
+        data.t_head = t_headd
+        data.wct = wctd
+        data.rp = rpd
+
+        well = HasanKabirAnn(qu_liq_m3day=qu_liq_m3dayd, d_i_m=d_i_md,
+                             d_o_m=d_o_md, h=hd, p_head=p_headd, 
+                             t_head=t_headd, wct=wctd, rp= rpd)
+        vr = float(well.down_pr())
+        data.results = vr
+
+        data.save()
+
+    else:
+        form = DataForm()
+        context['form'] = form
+
+    return render(request, 'hasankabir/index.html', context)
+
+# def index(request):
+#     return render(request, 'hasankabir/index.html')
+
+
